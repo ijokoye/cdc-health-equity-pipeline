ba92913040f180d62819997231c89748762cf4db
ijokoye <144378929+ijokoye@users.noreply.github.com>
2026-03-10 10:19:34 -0700

    Updated requirements and ingestion scripts

--- src/ingest/load_places_to_postgres.py
+++ src/ingest/load_places_to_postgres.py
@@ -1,10 +1,8 @@
 import pandas as pd
 from sqlalchemy import create_engine
-from urllib.parse import quote_plus
 
 CSV_PATH = "/Users/ijeomaokoye/IdeaProjects/cdc-health-equity-pipeline/data/raw/PLACES__Local_Data_for_Better_Health,_County_Data_2024_release_20260223.csv"
 
-PG_PASSWORD = "Amanda@!0o"
 DB_NAME = "health_equity"
 TABLE_NAME = "stg_places"
 
@@ -21,9 +19,8 @@ def main():
     df = pd.read_csv(CSV_PATH, low_memory=False)
     df.columns = clean_cols(df.columns)
 
-    password = quote_plus(PG_PASSWORD)  # ✅ encode special chars
     engine = create_engine(
-        f"postgresql+psycopg2://postgres:{password}@localhost:5432/{DB_NAME}"
+        f"postgresql+psycopg2://ijeomaokoye@localhost:5432/{DB_NAME}"
     )
 
     df.to_sql(TABLE_NAME, engine, if_exists="replace", index=False, chunksize=5000, method="multi")
--- src/ingest/load_svi_to_postgres.py
+++ src/ingest/load_svi_to_postgres.py
@@ -1,13 +1,10 @@
 import pandas as pd
 from sqlalchemy import create_engine
-from urllib.parse import quote_plus
-
 
 CSV = "/Users/ijeomaokoye/IdeaProjects/cdc-health-equity-pipeline/data/raw/SVI_2022_US_county (1).csv"
 
 DB_NAME = "health_equity"
 TABLE_NAME = "stg_svi"
-PG_PASSWORD = "Amanda@!0o"
 
 
 def clean_cols(cols):
@@ -21,19 +18,23 @@ def clean_cols(cols):
 
 
 def main():
-    # read file and clean then load to postgres
     df = pd.read_csv(CSV, dtype=str, low_memory=False)
     df.columns = clean_cols(df.columns)
 
     df["fips"] = df["fips"].astype(str).str.zfill(5)
 
-
-    password = quote_plus(PG_PASSWORD)
     engine = create_engine(
-        f"postgresql+psycopg2://postgres:{password}@localhost:5432/{DB_NAME}"
+        f"postgresql+psycopg2://ijeomaokoye@localhost:5432/{DB_NAME}"
     )
 
-    df.to_sql(TABLE_NAME, engine, if_exists='replace', index=False, chunksize=5000, method='multi')
+    df.to_sql(
+        TABLE_NAME,
+        engine,
+        if_exists="replace",
+        index=False,
+        chunksize=5000,
+        method="multi"
+    )
 
     print("Loaded SVI table successfully")
     print(df.columns)
@@ -41,14 +42,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
